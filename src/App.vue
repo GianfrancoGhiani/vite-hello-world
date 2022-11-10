87aebcc02fd57b5174a9e4ec166f4878402757c3
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2022-11-10 09:46:58 +0100

    hero added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+import AppHero from './components/AppHero.vue';
 import AppTitle from './components/AppTitle.vue';
+
 </script>
 
 <template>
   <header>
     <AppTitle />
+    <AppHero />
   </header>
 
 </template>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="wrapper">
+        <img src="../assets/it-takes-two-hero-banner-desktop.jpg" alt="it takes two">
+        <h2>it takes two</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHero',
+        data() {
+            return {
+                title: '',
+            }
+        },
+    }
+</script>
+
+<style lang="css" scoped>
+.wrapper{
+    position: relative;
+}
+img{
+    max-width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+h2{
+    position: absolute;
+    top: 0;
+    left: 15%;
+    color: white;
+    text-transform: capitalize;
+}
+
+</style>
